2df2004af9f59f4b5cc61e3dcba7de27cd7f769e
Matt Cadena <mcadena@andrew.cmu.edu>
2024-08-13 22:12:21 -0400

    Added basic title and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
+import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className={styles.main}>
+      <Image
+      fill={true}
+      src="/images/interns.jpg"
+      alt="image"
+      />
+
+      <div className={styles.sliderContainer}>
+        <div className={styles.slider}>
+          <p>Matt Cadena —</p>
+          <p>Matt Cadena —</p>
+        </div>
+      </div>
     </main>
   );
 }
