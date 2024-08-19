7c9c4faafb4ffc82be2ad39c95c76954355fe3fd
Matt Cadena <mcadena@andrew.cmu.edu>
2024-08-19 12:41:59 -0400

    changed divs to sections in experience page

--- src/components/work/experience/Experience.tsx
+++ src/components/work/experience/Experience.tsx
@@ -7,11 +7,30 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Experience() {
-  let containerRef = useRef(null);
+  let intro = useRef(null);
+  let ford = useRef(null);
+  let cmu = useRef(null);
+
+  let sections = [intro, ford, cmu];
+
+  useGSAP(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(sections, {
+      xPercent: -100 * (sections.length - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: ".container",
+        pin: true,
+        scrub: 1,
+        snap: 1 / (sections.length - 1),
+        end: "+=3500",
+      },
+    });
+  });
 
   return (
     <div className={styles.experience}>
-      <div className={styles.introPanel}>
+      <section className={styles.introPanel} ref={intro}>
         <p>
           Over the past two summers, I've gained valuable experience in both
           industry and academia. In 2024, I worked as a Software Engineering
@@ -21,10 +40,13 @@ export default function Experience() {
           Mellon University, where I focused on historical research related to
           Coup D'etats.
         </p>
-      </div>
-      <div className={styles.experiencePanel1}>
+      </section>
+      <section className={styles.experiencePanel1} ref={ford}>
         <h1>2024</h1>
-      </div>
+      </section>
+      <section className={styles.experiencePanel2} ref={cmu}>
+        <h1>2023</h1>
+      </section>
     </div>
   );
 }
