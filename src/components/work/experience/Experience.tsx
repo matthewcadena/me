6e5039eea7afa887100a777f62cfeaa658a3ba94
Matt Cadena <mcadena@andrew.cmu.edu>
2024-08-19 08:20:03 -0400

    added placeholder for second work section

--- src/components/work/experience/Experience.tsx
+++ src/components/work/experience/Experience.tsx
@@ -10,17 +10,20 @@ export default function Experience() {
   let containerRef = useRef(null);
 
   return (
-    <div className={styles.experience} ref={containerRef}>
+    <div className={styles.experience}>
       <div className={styles.introPanel}>
         <p>
           Over the past two summers, I've gained valuable experience in both
           industry and academia. In 2024, I worked as a Software Engineering
-          Intern at Ford in Dearborn, MI, contributing to products in the 
-          Enterprise Technology division.
-          The summer before, I was a Research Assistant at the Institute for
-          Politics and Strategy at Carnegie Mellon University, where I focused
-          on historical research related to Coup D'etats.
+          Intern at Ford in Dearborn, MI, contributing to products in the
+          Enterprise Technology division. The summer before, I was a Research
+          Assistant at the Institute for Politics and Strategy at Carnegie
+          Mellon University, where I focused on historical research related to
+          Coup D'etats.
         </p>
+      </div>
+      <div className={styles.experiencePanel1}>
+        <h1>2024</h1>
       </div>
     </div>
   );
