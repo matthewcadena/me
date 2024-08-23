2a5bfbf718adf6b811a5fcf5e5ec8cf6f75ef603
Matt Cadena <mcadena@andrew.cmu.edu>
2024-08-22 21:41:10 -0400

    changed about progress bar styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,6 @@ export default function Home() {
   useEffect(() => {
     const lenis = new Lenis();
 
-    lenis.on("scroll", (e) => {
-      console.log(e);
-    });
-
     function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,42 +1,47 @@
 export const projects = [
   {
     title: "CX Agent Portal",
-    description: "During my 2024 summer internship at Ford, I redesigned the agent portal for the company's customer service agents. The project involved creating an independent application with a user-friendly UI, incorporating feedback from agents to ensure an optimal experience. The solution was developed to operate without dependency on the existing content management system, which had a pending decommission date, ensuring long-term viability for Ford's customer service operations. Additionally, the application was hosted on Google Cloud Platform (GCP), and we implemented a CI/CD pipeline using Tekton to streamline development and deployment processes.",
+    description:
+      "During my 2024 summer internship at Ford, I redesigned the agent portal for the company's customer service agents. The project involved creating an independent application with a user-friendly UI, incorporating feedback from agents to ensure an optimal experience. The solution was developed to operate without dependency on the existing content management system, which had a pending decommission date, ensuring long-term viability for Ford's customer service operations. Additionally, the application was hosted on Google Cloud Platform (GCP), and we implemented a CI/CD pipeline using Tekton to streamline development and deployment processes.",
     src: "agent-portal.png",
     link: "https://artisan.ford.com/team-shelby",
     linkText: "Read more about Team Shelby",
-    color: "#796b74"
+    color: "#796b74",
   },
   {
     title: "Jargon Garden",
-    description: "During a team hackathon at my Ford internship, our team created 'Jargon Gardon,' an app that humorously transforms phrases from the user into corporate buzzword gold. Built with React and TypeScript, and powered by Ford's LLM which we connected to through the in-house API, the project was as much about having fun as it was about developing our technical skills. Our playful approach paid off—we won the popular vote for best hackathon project, even though we were the only team made up entirely of interns.",
+    description:
+      "During a team hackathon at my Ford internship, our team created 'Jargon Gardon,' an app that humorously transforms phrases from the user into corporate buzzword gold. Built with React and TypeScript, and powered by Ford's LLM which we connected to through the in-house API, the project was as much about having fun as it was about developing our technical skills. Our playful approach paid off—we won the popular vote for best hackathon project, even though we were the only team made up entirely of interns.",
     src: "jargon-garden.jpg",
     link: "https://artisan.ford.com/team-shelby",
     linkText: "Read more about Team Shelby",
-    color: "#8d7f89"
+    color: "#8d7f89",
   },
   {
     title: "Gotham City Tracker",
-    description: "As the main project in my 67-272: Application Design and Development, I developed 'Gotham City Department Tracker,' a full-stack CRUD application using Ruby on Rails, React, and MySQL. I led the entire design and development process, starting with crafting user stories crafted from a list of functional requirements. Following an incremental, agile approach, I built a custom API, implemented secure authentication, and designed a dynamic UI. The project leveraged MVC architecture for code organization, with MySQL handling database management.",
+    description:
+      "As the main project in my 67-272: Application Design and Development, I developed 'Gotham City Department Tracker,' a full-stack CRUD application using Ruby on Rails, React, and MySQL. I led the entire design and development process, starting with crafting user stories crafted from a list of functional requirements. Following an incremental, agile approach, I built a custom API, implemented secure authentication, and designed a dynamic UI. The project leveraged MVC architecture for code organization, with MySQL handling database management.",
     src: "gcpd-animation.gif",
     link: "https://github.com/67-272-Students-Spring-2024/gcpd-phase-4-matthewcadena",
     linkText: "Source code",
-    color: "#a2939d"
+    color: "#a2939d",
   },
   {
     title: "NBA Tic Tac Toe",
-    description: "As a personal project, I developed 'NBA Tic-Tac-Toe,' a full-stack web application that brings an interactive twist to the classic game. Using Django and Python, I connected the app to a PostgreSQL database to pull in NBA data and wrote dynamic queries to validate user responses. I also had fun experimenting with Google’s Image-Search API to fetch player headshots on the fly, adding a unique visual element to the game.",
+    description:
+      "As a personal project, I developed 'NBA Tic-Tac-Toe,' a full-stack web application that brings an interactive twist to the classic game. Using Django and Python, I connected the app to a PostgreSQL database to pull in NBA data and wrote dynamic queries to validate user responses. I also had fun experimenting with Google’s Image-Search API to fetch player headshots on the fly, adding a unique visual element to the game.",
     src: "nba-tac-toe.gif",
     link: "https://www.youtube.com/watch?v=v2mr_DU-5Fo",
     linkText: "Video demo",
-    color: "#b6a7b2"
+    color: "#b6a7b2",
   },
   {
     title: "Personal Website",
-    description: "For my personal website, I leveraged my experience with React, TypeScript, and Jotai from my internship to craft a dynamic Next.js web app. Integrating Framer Motion and GSAP, I created a captivating display that highlights my exploration of new frontend and design techniques. The result is a visually engaging and technically sophisticated site that reflects my growth and creativity in web development.",
+    description:
+      "For this website, I leveraged my experience with React, TypeScript, and Jotai from my internship to craft a dynamic Next.js web app. Integrating Framer Motion and GSAP, I created a captivating display that highlights my exploration of new frontend and design techniques. The result is a visually engaging and technically sophisticated site that reflects my growth and creativity in web development.",
     src: "website-landing.png",
     link: "https://github.com/matthewcadena/me",
     linkText: "Source code",
-    color: "#cbbbc7"
-  }
-]
+    color: "#cbbbc7",
+  },
+];
